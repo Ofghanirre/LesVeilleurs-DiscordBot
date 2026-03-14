4b88bd5c6e7d62dca79dced039d2f5906494afde
Ofghanirre <ofghanirre@gmail.com>
2026-03-14 20:51:17 +0100

    feat(project): add ClearCommand.

---
+++ src/main/java/fr/ofghanirre/lesveilleursdiscordbot/commands/ClearCommand.java
@@ -0,0 +1,53 @@
+package fr.ofghanirre.lesveilleursdiscordbot.commands;
+
+import fr.ofghanirre.lesveilleursdiscordbot.guildcontent.GuildContentManager;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class ClearCommand implements ISlashCommand {
+    private final GuildContentManager manager;
+
+    public ClearCommand(GuildContentManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public String getName() {
+        return "clear";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Clear the server state, deleting bot-made channels and categories";
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Guild guild = event.getGuild();
+        if (guild == null) {
+            event.reply("Could not find server").queue();
+            return;
+        }
+
+        manager.getContent(guild.getIdLong()).ifPresent(content -> {
+            if (content.voiceChannelId() != 0) {
+                VoiceChannel vc = guild.getVoiceChannelById(content.voiceChannelId());
+                if (vc != null) vc.delete().queue();
+            }
+
+            if (content.categoryId() != 0) {
+                Category cat = guild.getCategoryById(content.categoryId());
+                if (cat != null) {
+                    cat.delete().queue();
+                }
+            }
+        });
+
+        manager.remove(guild.getIdLong());
+        manager.save();
+
+        event.reply("Successfully cleared the %s discord server ✅".formatted(guild.getName())).queue();
+    }
+}
