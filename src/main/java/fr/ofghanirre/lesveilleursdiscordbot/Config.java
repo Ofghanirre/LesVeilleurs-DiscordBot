3fb5020b02b577cf5ab6d698e8ea46035efe0463
Ofghanirre <ofghanirre@gmail.com>
2026-03-14 21:03:06 +0100

    feat(project): update Config to pass by env value instead of config files.

--- src/main/java/fr/ofghanirre/lesveilleursdiscordbot/Config.java
+++ src/main/java/fr/ofghanirre/lesveilleursdiscordbot/Config.java
@@ -1,35 +1,20 @@
 package fr.ofghanirre.lesveilleursdiscordbot;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Optional;
-import java.util.Properties;
 
 public final class Config {
-    private static final String CONFIG_FILE_NAME = "config.properties";
     private static final String TOKEN_KEY = "token";
+    private static final String ENV_TOKEN_KEY = "DISCORD_TOKEN";
 
     public static Optional<String> extractToken() {
-        try {
-            var token = getToken();
-            if (token.isEmpty()) {
-                System.out.println(String.format("ERROR: Could not find '%s' parameter within the '%s' file.", TOKEN_KEY, CONFIG_FILE_NAME));
-            }
-            return token;
-        } catch (IOException e) {
-            throw new RuntimeException(String.format("Could not find '%s' file.", CONFIG_FILE_NAME));
+        var token = getTokenFromEnv();
+        if (token.isEmpty()) {
+            System.out.printf("ERROR: Could not find '%s' parameter within the env.%n", TOKEN_KEY);
         }
+        return token;
     }
 
-    private static Optional<String> getToken() throws IOException {
-        InputStream input = Main.class
-                .getClassLoader()
-                .getResourceAsStream(CONFIG_FILE_NAME);
-
-        Properties props = new Properties();
-        props.load(input);
-
-        String token = props.getProperty(TOKEN_KEY);
-        return Optional.ofNullable(token);
+    private static Optional<String> getTokenFromEnv() {
+        return Optional.ofNullable(System.getenv(ENV_TOKEN_KEY));
     }
 }
